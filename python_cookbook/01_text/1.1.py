cd82f545ec01585767bc63cf847ab5af2ed1c0c1
liao_zd <liao_zd@hotmail.com>
2015-01-31 22:53:05 +0800

    9.1 Synchronizing All Methods in an Object

---
+++ python_cookbook/01_text/1.1.py
@@ -0,0 +1,48 @@
+__author__ = 'liao'
+
+# 1.1 Processing a String One Character at a Time
+print "C1.1"
+thestring = "abcdefghijklmn"
+
+# list comprehension
+resultA = [ c+"." for c in thestring]
+
+import sets
+magic_chars = sets.Set('abracadabra')
+poppins_chars = sets.Set('supercalifgrailisticexpialidocious')
+set_intersection = magic_chars & poppins_chars
+print ''.join(set_intersection)
+
+# 1.2 Converting Between Characters and Numeric Codes
+print "C1.2"
+print ord('a')
+print chr(97)
+print ord(u'\u2020')
+print repr(unichr(8224))
+print map(ord, 'ciao')
+print ''.join(map(chr, range(97,120)))
+
+# 1.3 Testing Whether an Object Is String-like
+print "C1.3"
+def isAString(anobj):
+    return isinstance(anobj, basestring)
+
+
+def isExactlyAString(anobj):
+    # unicode will fail
+    return type(anobj) is type('')
+
+u_str = unicode('abcde')
+
+print isAString(u_str), isExactlyAString(u_str)
+
+
+def isStringLike(anobj):
+    # this method is because basestring does not include "UserString"
+    try:
+        anobj + ''
+    except:
+        return False
+    else:
+        return True
+
