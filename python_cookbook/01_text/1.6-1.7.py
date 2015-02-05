cc2dcae0e80a6c4deaea6f505c6cf79a6befdbcc
liao_zd <liao_zd@hotmail.com>
2015-02-05 23:28:29 +0800

    # 1.7 Reversing a String by Words or Characters

--- python_cookbook/01_text/1.6-1.7.py
+++ python_cookbook/01_text/1.6-1.7.py
@@ -17,3 +17,33 @@ print largeString
 import operator
 largeString = reduce(operator.add, pieces, '')
 print largeString
+
+
+# 1.7 Reversing a String by Words or Characters
+astring = 'abc  def ghi'
+revchars = astring[::-1]
+print revchars
+
+revwords = astring.split()
+print revwords
+revwords.reverse()          # revers the list in place
+print revwords
+revwords = ' '.join(revwords)
+print revwords
+
+# or compact "one-liners"
+revwords = ' '.join(astring.split()[::-1])
+print "one-liners:", revwords
+
+# preserving untouched the intermediate whitespace
+import re
+revwords = re.split(r'(\s+)', astring)
+print revwords
+revwords.reverse()
+print revwords
+revwords = ''.join(revwords)
+print revwords
+
+# Bad one-liner code
+revwords = ''.join(re.split(r'(\s+)', astring)[::-1])
+print revwords
