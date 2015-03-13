f5969ddcf4203fcf201cb4ae9b8c2ef768b5aef1
liaozd <liao_zd@hotmail.com>
2015-03-13 10:11:05 +0800

    chap2

---
+++ lightweightdjango/chap2/placeholder/placeholder.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+from django.conf import settings
+# This is project template
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '(j=!b1$2&4fpz*4#pz1=m2r37ue2$v=gbtn*@3ig6a8n1%z2^b')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+# django-admin.py startproject foo --template=project_name
+# to start a new project by this template
+
+# hostname $ export DEBUG=off
+# hostname $ export ALLOWED_HOSTS=localhost,example.com
+# hostname $ python hello.py runserver
