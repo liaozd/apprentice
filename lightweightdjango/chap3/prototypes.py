2afb3d6ba44f48a6c287fb9ff6b61217e16dab3e
liaozd <liao_zd@hotmail.com>
2015-04-29 07:08:52 +0800

    fix - debug: no compress tag

--- lightweightdjango/chap3/prototypes.py
+++ lightweightdjango/chap3/prototypes.py
@@ -5,17 +5,12 @@ from django.conf import settings
 
 BASE_DIR = os.path.dirname(__file__)
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
 
 settings.configure(
     DEBUG=True,
-    TEMPLATE_DEBUG=False,
     SECRET_KEY='b0mqvak1p2sqm6p#+8o8fyxf+ox(le)8&jh_5^sxa!=7!+wxj0',
     ROOT_URLCONF='sitebuilder.urls',
     MIDDLEWARE_CLASSES=(),
-    ALLOWED_HOSTS=ALLOWED_HOSTS,
     INSTALLED_APPS=(
         'django.contrib.staticfiles',
         'django.contrib.webdesign',
@@ -26,13 +21,13 @@ settings.configure(
     SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
     SITE_OUTPUT_DIRECTORY=os.path.join(BASE_DIR, '_build'),
     STATIC_ROOT=os.path.join(BASE_DIR, '_build', 'static'),
-    # STATICFILES_STORAGE='django.contrib.staticfiles.storage.CachedStaticFilesStorage',
-    STATIC_FINDERS=(
+    STATICFILES_FINDERS=(
         'django.contrib.staticfiles.finders.FileSystemFinder',
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
         'compressor.finders.CompressorFinder',
-    )
+    ),
 )
+
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
