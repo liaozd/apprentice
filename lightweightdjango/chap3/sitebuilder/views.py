fde6afa91faec35203fc4bd7f037d8b974b1732e
liaozd <liao_zd@hotmail.com>
2015-04-30 10:51:12 +0800

    debuging: pricing page DRY

--- lightweightdjango/chap3/sitebuilder/views.py
+++ lightweightdjango/chap3/sitebuilder/views.py
@@ -1,9 +1,11 @@
+import json
 import os
 
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
 
@@ -19,6 +21,16 @@ def get_page_or_404(name):
             raise Http404('Page Not Found')
     with open(file_path, 'r') as f:
         page = Template(f.read())
+    print(page.nodelist)
+    print("page.nodelist number:", len(page.nodelist))
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            print("node.name is context: ", node)
+            meta = page.nodelist.pop(i)
+            print("meta:", meta)
+            break
+    page._meta = meta
     return page
 
 
@@ -31,4 +43,8 @@ def page(request, slug='index'):
         'slug': slug,
         'page': page,
     }
+    if page._meta is not None:
+        meta = page._meta.render(Context())
+        extra_context = json.loads(meta)
+        context.update(extra_context)
     return render(request, 'page.html', context)
