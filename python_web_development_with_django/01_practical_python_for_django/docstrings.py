53d98453d31a33cb8998c63c16b70e6591ee227f
liaozd <liao_zd@hotmail.com>
2015-09-23 17:29:09 +0800

    pwdwd chap2

---
+++ python_web_development_with_django/01_practical_python_for_django/docstrings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""docstrings.py -- sample module demonstrating documentation strings"""
+
+
+class Foo(object):
+    """Foo() - empty class .. """
+
+    def bar(x):
+        """bar(x) - function docstring for bar, print out its arg 'x'"""
+        print x
+
