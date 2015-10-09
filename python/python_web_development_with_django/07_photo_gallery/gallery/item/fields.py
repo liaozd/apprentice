65c97407d478c244fbab126a741dc2a1d5fefa78
liaozd <liao_zd@hotmail.com>
2015-10-09 14:47:58 +0800

    orgornize folders

--- python/python_web_development_with_django/07_photo_gallery/gallery/item/fields.py
+++ python/python_web_development_with_django/07_photo_gallery/gallery/item/fields.py
@@ -2,6 +2,7 @@ from django.db.models.fields.files import ImageField, ImageFieldFile
 from PIL import Image
 import os
 
+
 def _add_thumb(s):
     """
     Modifies a string (filename, URL) containing an image filename, to insert
@@ -9,13 +10,20 @@ def _add_thumb(s):
     """
     parts = s.split(".")
     parts.insert(-1, "thumb")
-    if parts[-1].lower() not in ['jpeg', 'jpg']
+    if parts[-1].lower() not in ['jpeg', 'jpg']:
         parts[-1] = 'jpg'
     return ".".join(parts)
+
 
 class ThumbnailImageFieldFile(ImageFieldFile):
     def _get_thumb_path(self):
         return _add_thumb(self.path)
+    thumb_path = property(_get_thumb_path)
+
+    def _get_thumb_url(self):
+        return _add_thumb(self.url)
+    thumb_url = property(_get_thumb_url)
+
 
 class ThumbnailImageField(ImageField):
 
