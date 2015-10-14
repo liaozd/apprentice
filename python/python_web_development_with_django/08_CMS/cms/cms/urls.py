87d6a6dc5030d130260f102eb45a10cfaad60a16
liaozd <liao_zd@hotmail.com>
2015-10-14 17:49:33 +0800

    chap 8 cms

--- python/python_web_development_with_django/08_CMS/cms/cms/urls.py
+++ python/python_web_development_with_django/08_CMS/cms/cms/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'cms.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns('',
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^story/', include('story.urls')),
+)
 
-    url(r'^admin/', include(admin.site.urls)),
-]
