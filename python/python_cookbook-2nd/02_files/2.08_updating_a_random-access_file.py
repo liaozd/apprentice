bc3a8340023d2169ae0e43ac670d38c8d1ceeb30
liaozd <liao_zd@hotmail.com>
2015-11-01 13:09:07 +0800

    cookbook chap 2.28

--- python/python_cookbook-2nd/02_files/2.08_updating_a_random-access_file.py
+++ python/python_cookbook-2nd/02_files/2.08_updating_a_random-access_file.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
 import struct
+
+format_string = '8l'
+thefile = open('somebinfile', 'r+b')
+record_size = struct.calcsize(format_string)
+record_number = 6
+thefile.seek(record_size * record_number)
+buffer = thefile.read(record_size)
+fields = list(struct.unpack(format_string, buffer))
+# Perform computations, suitably modifying fields, then:
+buffer = struct.pack(format_string, *fields)
+thefile.seek(record_size * record_number)
+thefile.write(buffer)
+thefile.close()
