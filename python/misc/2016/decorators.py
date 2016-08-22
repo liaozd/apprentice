9804e63835720d47aa2207780cd2471c4a404149
liaozd <liao_zd@hotmail.com>
2016-08-22 21:43:03 +0800

    decorators

---
+++ python/misc/2016/decorators.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+# https://wiki.python.org/moin/PythonDecoratorLibrary
+
+# Creating Well-Behaved Decorators / "Decorator decorator"
+print '# Creating Well-Behaved Decorators / "Decorator decorator"'
+
+
+def simple_decorator(decorator):
+    def new_decorator(f):
+        g = decorator(f)
+        g.__name__ = f.__name__
+        g.__doc__ = f.__doc__
+        g.__dict__.update(f.__dict__)
+        return g
+    new_decorator.__name__ = decorator.__name__
+    new_decorator.__doc__ = decorator.__doc__
+    new_decorator.__dict__.update(decorator.__dict__)
+    return new_decorator
+
+
+@simple_decorator
+def my_simple_logging_decorator(func):
+    def you_will_never_see_this_name(*args, **kwargs):
+        print 'Calling {}'.format(func.__name__)
+        return func(*args, **kwargs)
+    return you_will_never_see_this_name
+
+
+@my_simple_logging_decorator
+def double(x):
+    """Doubles a number."""
+    return 2 * x
+
+assert double.__name__ == 'double'
+assert double.__doc__ == 'Doubles a number.'
+print double(155)
+
+
+# Property Definition
+print '# Property Definition'
+
+import sys
+
+
+def propget(func):
+    locals = sys._getframe(1).f_locals
+    name = func.__name__
+    prop = locals.get(name)
+    if not isinstance(prop, property):
+        prop = property(func, doc=func.__doc__)
+    else:
+        doc = prop.__doc__ or func.__doc__
+        prop = property(func, prop.fset, prop.fdel, doc)
+    return prop
+
+
+def propset(func):
+    locals = sys._getframe(1).f_locals
+    name = func.__name__
+    prop = locals.get(name)
+    if not isinstance(prop, property):
+        prop = property(None, func, doc=func.__doc__)
+    else:
+        doc = prop.__doc__ or func.__doc__
+        prop = property(prop.fget, func, prop.fdel, doc)
+    return prop
+
+
+def propdel(func):
+    locals = sys._getframe(1).f_locals
+    name = func.__name__
+    prop = locals.get(name)
+    if not isinstance(prop, property):
+        prop = property(None, None, func, doc=func.__doc__)
+    else:
+        prop = property(prop.fget, prop.fset, func, prop.__doc__)
+    return prop
+
+class Example(object):
+
+    @propget
+    def myattr(self):
+        return self._half * 2
+
+    @propset
+    def myattr(self, value):
+        self._half = value / 2
+
+    @propdel
+    def myattr(self):
+        del self._half
