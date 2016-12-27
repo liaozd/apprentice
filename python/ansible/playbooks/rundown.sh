930cef632d77cf0b512a8c4eb89351b510332646
liaozd <liao_zd@hotmail.com>
2016-12-27 22:37:43 +0800

    start a https in a container

--- python/ansible/playbooks/rundown.sh
+++ python/ansible/playbooks/rundown.sh
@@ -4,7 +4,10 @@
 docker build -t ubuntu-ssh-server .
 
 # Start a test docker server
-docker run -d -p 2222:22 ubuntu-ssh-server
+docker run -d -p 2222:22 -p 8080:80 -p 8443:443 ubuntu-ssh-server
+
+# ssh into the ubuntu-ssh-server
+sshpass -p 'screencast' ssh -p 2222 root@localhost
 
 # use hosts file
 ansible docker-server -i hosts -m command -a uptime
@@ -17,3 +20,9 @@ ansible docker-server -s -m apt -a "name=nginx update_cache=yes"
 
 # host pi server
 ansible pi-server -m ping
+
+# Create TLS certificate
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -subj /CN=localhost -keyout files/nginx.key -out files/nginx.crt
+
+# Start a tls
+ansible-playbook web-tls.yml
