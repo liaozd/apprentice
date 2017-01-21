d77d2acb9c9283595cd2c7b6283064e0c46822c2
liaozd <liao_zd@hotmail.com>
2017-01-21 20:54:17 +0800

    python cookbook chap 2.15-19

---
+++ python/python_cookbook-2nd/02_files/2.16_walking_directory_trees.py
@@ -0,0 +1,23 @@
+import os, fnmatch
+
+
+def all_files(root, patterns='*', single_level=False, yield_folders=False):
+    # Expand patterns from semicolon-separated string to list
+    patterns = patterns.split(';')
+    for path, subdirs, files in os.walk(root):
+        if yield_folders:
+            files.extend(subdirs)
+        files.sort()
+        for name in files:
+            for pattern in patterns:
+                if fnmatch.fnmatch(name, pattern):
+                    yield os.path.join(path, name)
+                    break
+            if single_level:
+                break
+
+thefiles = list(all_files('/tmp'))
+print thefiles
+
+for path in all_files('/tmp'):
+    print path
