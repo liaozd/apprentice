ed126411d3963189ee30f33a5c1bf02669211369
Liao Zhuodi <liao_zd@hotmail.com>
2017-07-31 17:29:08 +0800

    Tutorial 6: ViewSets & Routers - Using Routers

--- python/django-rest-framework/tutorial/urls.py
+++ python/django-rest-framework/tutorial/urls.py
@@ -4,15 +4,17 @@ from django.contrib import admin
 from rest_framework import routers
 
 from tutorial.quickstart import views
+from snippets import views as snippet_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'snippets', snippet_views.SnippetViewSet)
+router.register(r'snippet-users', snippet_views.UserViewSet)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^snippets/', include('snippets.urls')),
 ]
