a158202b4d25aede764312514ea0e8c72be08d68
Liao Zhuodi <liao_zd@hotmail.com>
2017-08-28 08:08:33 +0800

    docker gui start firefox

--- docker/rundown.sh
+++ docker/rundown.sh
@@ -10,3 +10,38 @@ sudo docker run --detach \
     --volume /srv/gitlab/logs:/var/log/gitlab \
     --volume /srv/gitlab/data:/var/opt/gitlab \
     gitlab/gitlab-ce:latest
+
+# TECHNIQUE 21 Retain your container's bash history
+docker run -e HISTFILE=/root/.bash_history -v $HOME/.bash_history:/root/.bash_history -ti ubuntu /bin/bash
+
+# TECHNIQUE 25 Dev tools container
+docker run -t -i \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=$DISPLAY \
+--net=host --ipc=host \
+-v /opt/workspace:/home/dockerinpractice \
+dockerinpractice/docker-dev-tools-image # ubuntu
+
+## 1
+# Firefox docker
+FROM ubuntu:14.04
+
+RUN apt-get update
+RUN apt-get install -y firefox
+
+RUN groupadd -g 1000 neo
+RUN useradd -d /home/neo -s /bin/bash -m neo -u 1000 -g 1000
+USER neo
+ENV home /home/neo
+CMD /usr/bin/firefox
+
+## 2
+docker build -t gui .
+
+## 3
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=$DISPLAY \
+-h $HOSTNAME \
+-v $HOME/.Xauthority:/home/$USER/.Xauthority \
+gui
