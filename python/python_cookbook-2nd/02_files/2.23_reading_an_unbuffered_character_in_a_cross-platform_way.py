100f40a2391a8fb46f01f0430fcfce9784b0adbc
Liao Zhuodi <liao_zd@hotmail.com>
2017-10-12 22:05:44 +0800

    python cookbook2n 2.23

---
+++ python/python_cookbook-2nd/02_files/2.23_reading_an_unbuffered_character_in_a_cross-platform_way.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+try:
+    from msvcrt import getch
+except ImportError:
+    def getch():
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
