edae3659fbc554e766ac90549f6309553cd6d022
Liao Zhuodi <liao_zd@hotmail.com>
2017-10-21 21:03:51 +0800

    backbone: update

--- web/backbone/backbone_tutorial_blogroll_app/script.js
+++ web/backbone/backbone_tutorial_blogroll_app/script.js
@@ -37,6 +37,29 @@ var BlogView = Backbone.View.extend({
   initialize: function () {
     this.template = _.template($('.blogs-list-template').html());
   },
+  events: {
+    'click .edit-blog': 'edit',
+    'click .update-blog': 'update'
+  },
+  edit: function () {
+    $('.delete-blog').hide();
+    $('.edit-blog').hide();
+    $('.update-blog').show();
+    $('.cancel').show();
+
+    var author = this.$('.author').html();
+    var title = this.$('.title').html();
+    var url = this.$('.url').html();
+
+    this.$('.author').html('<input type="text" class="form-control author-update" value="' + author + '">');
+    this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '">');
+    this.$('.url').html('<input type="text" class="form-control url-update" value="' + url + '">');
+  },
+  update: function () {
+    this.model.set('author', $('.author-update').val());
+    this.model.set('title', $('.title-update').val());
+    this.model.set('url', $('.url-update').val());
+  },
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
@@ -49,7 +72,13 @@ var BlogsView =Backbone.View.extend({
   model: blogs,
   el: $('.blogs-list'),
   initialize: function () {
+    var self = this;
     this.model.on('add', this.render, this);
+    this.model.on('change', function () {
+      setTimeout(function () {
+        self.render();
+      }, 30);
+    }, this);
   },
   render: function () {
     var self = this;
